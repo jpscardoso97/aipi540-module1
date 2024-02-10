28bc03545c316187b65a69b51a4579adf6bfedc7
zihanxing <zachxing@qq.com>
2024-02-10 17:30:52 +0000

    Update image cropping script

--- scripts/segmentation/segment_process.py
+++ scripts/segmentation/segment_process.py
@@ -1,26 +1,31 @@
 import os
 import sys
 
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+from groundingdino.util.inference import Model
+
+import torch
+from torchvision.ops import box_convert
+import numpy as np
+from PIL import Image
+import supervision as sv
+
+import cv2
+
+# Define the path to the repository
 HOME = os.getcwd()
-print(HOME)
 
 home_dir = HOME
 repo_dir = "GroundingDINO"
 os.chdir(os.path.join(home_dir, repo_dir))
-print(os.getcwd())
 
 sys.path.append(os.path.join(home_dir, repo_dir))
 
-
-from groundingdino.util.inference import load_model, load_image, predict, annotate
-from groundingdino.util.inference import Model
-
+# Load the model
 CONFIG_PATH = os.path.join(HOME, "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py")
 WEIGHTS_NAME = "groundingdino_swint_ogc.pth"
 WEIGHTS_PATH = os.path.join(HOME, "weights", WEIGHTS_NAME)
 print(WEIGHTS_PATH, "; exist:", os.path.isfile(WEIGHTS_PATH))
-
-
 
 model = load_model(CONFIG_PATH, WEIGHTS_PATH)
 
@@ -32,11 +37,6 @@ print(GROUNDING_DINO_CHECKPOINT_PATH, "; exist:", os.path.isfile(GROUNDING_DINO_
 
 grounding_dino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH)
 
-import torch
-from torchvision.ops import box_convert
-import numpy as np
-from PIL import Image
-import supervision as sv
 
 TEXT_PROMPT = "snake"
 BOX_TRESHOLD = 0.25
@@ -56,7 +56,7 @@ def get_bounding_box(path):
   annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
 
 #   %matplotlib inline
-  sv.plot_image(annotated_frame, (16, 16))
+  # sv.plot_image(annotated_frame, (16, 16))
 
   return boxes[0].numpy()
 
@@ -87,26 +87,34 @@ def get_cropped_image(img_path, box):
     y1 = center_y - height // 2
     y2 = center_y + height // 2
 
-    print("Cropping image from", (x1, y1), "to", (x2, y2))
+    # print("Cropping image from", (x1, y1), "to", (x2, y2))
 
     cropped_img = img[y1:y2, x1:x2]
 
-    cv2_imshow(cropped_img)
+    # cv2.imshow(cropped_img)
 
     return cropped_img
 
 # iterate over all images in the data/raw folder and subfolders and crop the snake
-import os
-import cv2
-
 RAW_DATA_PATH = os.path.join(HOME, "../../data/raw")
 
-# WIP - TODO: iterate over all the images and crop the snakes into new files. 
+flag = 0
 for root, dirs, files in os.walk(RAW_DATA_PATH):
     for file in files:
         img_path = os.path.join(root, file)
         print(img_path)
-        box = get_bounding_box(img_path)
-        cropped_img = get_cropped_image(img_path, box)
-        cv2.imwrite(img_path, cropped_img)
+        try:
+          box = get_bounding_box(img_path)
+          cropped_img = get_cropped_image(img_path, box)
+          cv2.imwrite(img_path, cropped_img)
+          flag += 1
+          print('Success:', flag)
+        except:
+          print("Error in writing file:", img_path)
+          flag += 1
+          # save the error in a log file
+          with open("error_log.txt", "a") as file:
+            file.write("Error in writing file: " + img_path + "\n")
+          continue
+        
             
